b51fe36f2a252d58cff11dafe2f09a7d2f4799a2
Kelan Ige <krige89@gmail.com>
2018-02-21 21:08:46 -0500

    Threads implements, mutex pending, compiles successfully

--- coop.cpp
+++ coop.cpp
@@ -6,37 +6,38 @@
 #include <fstream>
 #include <thread>
 #include <mutex>
+#include <vector>
 
 // Declare the Chicken class
 class Chicken {
-	private:
-		unsigned int count;
-		std::chrono::time_point<std::chrono::steady_clock> spawn_time;
-	public:
-		Chicken();
-		Chicken(unsigned int, std::chrono::time_point<std::chrono::steady_clock>);
-		unsigned int count_chickens();
-		std::chrono::time_point<std::chrono::steady_clock> get_time();
-		void set_chickens(unsigned int);
-		void set_time(std::chrono::time_point<std::chrono::steady_clock>);
-		void  lay_eggs(class Egg *, std::chrono::time_point<std::chrono::steady_clock>);
-		float sell_chickens(unsigned int);
+  private:
+  unsigned int count;
+  std::chrono::time_point<std::chrono::steady_clock> spawn_time;
+  public:
+  Chicken();
+  Chicken(unsigned int, std::chrono::time_point<std::chrono::steady_clock>);
+  unsigned int count_chickens();
+  std::chrono::time_point<std::chrono::steady_clock> get_time();
+  void set_chickens(unsigned int);
+  void set_time(std::chrono::time_point<std::chrono::steady_clock>);
+  void  lay_eggs(class Egg *, std::chrono::time_point<std::chrono::steady_clock>);
+  float sell_chickens(unsigned int);
 };
 
 //Declare the Egg class
 class Egg {
-	private:
-		unsigned int count;
-		std::chrono::time_point<std::chrono::steady_clock> spawn_time;
-	public:
-		Egg();
-		Egg(unsigned int, std::chrono::time_point<std::chrono::steady_clock>);
-		unsigned int count_eggs();
-		std::chrono::time_point<std::chrono::steady_clock> get_time();
-		void set_eggs(unsigned int);
-		void set_time(std::chrono::time_point<std::chrono::steady_clock>);
-		void hatch(class Chicken *, std::chrono::time_point<std::chrono::steady_clock>);
-		float sell_eggs(unsigned int);
+  private:
+  unsigned int count;
+  std::chrono::time_point<std::chrono::steady_clock> spawn_time;
+  public:
+  Egg();
+  Egg(unsigned int, std::chrono::time_point<std::chrono::steady_clock>);
+  unsigned int count_eggs();
+  std::chrono::time_point<std::chrono::steady_clock> get_time();
+  void set_eggs(unsigned int);
+  void set_time(std::chrono::time_point<std::chrono::steady_clock>);
+  void hatch(class Chicken *, std::chrono::time_point<std::chrono::steady_clock>);
+  float sell_eggs(unsigned int);
 };
 
 // Declare mutex
@@ -55,38 +56,38 @@ std::mutex difference_mutex;
 //    : slang : petty details
 // 5  : slang : a young male homosexual
 Chicken::Chicken() {
-	count = 0;
+  count = 0;
 }
 
 Chicken::Chicken(unsigned int c, std::chrono::time_point<std::chrono::steady_clock> t) {
-	count = c;
-	spawn_time = t;
+  count = c;
+  spawn_time = t;
 }
 
 unsigned int Chicken::count_chickens() {
-	return count;
+  return count;
 }
 
 std::chrono::time_point<std::chrono::steady_clock> Chicken::get_time() {
-	return spawn_time;
+  return spawn_time;
 }
 
 void Chicken::set_chickens(unsigned int c) {
-	count = c;
+  count = c;
 }
 
 void Chicken::set_time(std::chrono::time_point<std::chrono::steady_clock> t) {
-	spawn_time = t;
+  spawn_time = t;
 }
 
 void Chicken::lay_eggs(class Egg * e, std::chrono::time_point<std::chrono::steady_clock> t) {
-	e->set_eggs(count);
-	e->set_time(t);
+  e->set_eggs(count);
+  e->set_time(t);
 }
 
 float Chicken::sell_chickens(unsigned int num) {
-	count -= num;
-	return (float) 10*num;
+  count -= num;
+  return (float) 10*num;
 }
 
 // Define Egg
@@ -99,167 +100,167 @@ float Chicken::sell_chickens(unsigned int num) {
 // 2  : something resembling an egg
 // 3  : person, sort; a good egg
 Egg::Egg() {
-	count = 0;
+  count = 0;
 }
 
 Egg::Egg(unsigned int c, std::chrono::time_point<std::chrono::steady_clock> t) {
-	count = c;
-	spawn_time = t;
+  count = c;
+  spawn_time = t;
 }
 
 unsigned int Egg::count_eggs() {
-	return count;
+  return count;
 }
 
 std::chrono::time_point<std::chrono::steady_clock> Egg::get_time() {
-	return spawn_time;
+  return spawn_time;
 }
 
 void Egg::set_eggs(unsigned int c) {
-	count = c;
+  count = c;
 }
 
 void Egg::set_time(std::chrono::time_point<std::chrono::steady_clock> t) {
-	spawn_time = t;
+  spawn_time = t;
 }
 
 void Egg::hatch(class Chicken * c, std::chrono::time_point<std::chrono::steady_clock> t) {
-	c->set_chickens(count);
-	c->set_time(t);
+  c->set_chickens(count);
+  c->set_time(t);
 }
 
 float Egg::sell_eggs(unsigned int num) {
-	count = count - num;
-	return 0.25*(float)num;
+  count = count - num;
+  return 0.25*(float)num;
 }
 
 // Declare and define chicken_manager
 void chicken_manager(std::list<class Chicken *> ckn_list) {
-	std::chrono::time_point<std::chrono::steady_clock> now;
-	while (ckn_list.size() > 0) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(5));
-		while (!chicken_mutex.try_lock()) {
-			std::this_thread::sleep_for(std::chrono::microseconds(500));
-		}
-		std::cout << "From chicken_manager thread: " << ckn_list.size()  << std::endl;
-		ckn_list.front()->sell_chickens(ckn_list.front()->count_chickens() - 15);
-		chicken_mutex.unlock();
-	}
+  std::chrono::time_point<std::chrono::steady_clock> now;
+  while (ckn_list.size() > 0) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  while (!chicken_mutex.try_lock()) {
+  std::this_thread::sleep_for(std::chrono::microseconds(500));
+  }
+  std::cout << "From chicken_manager thread: " << ckn_list.size()  << std::endl;
+  ckn_list.front()->sell_chickens(ckn_list.front()->count_chickens() - 15);
+  chicken_mutex.unlock();
+  }
 }
 
 // Declare and define egg_manager
 void egg_manager(std::list<class Egg *> eg_list) {
-	std::chrono::time_point<std::chrono::steady_clock> now;
-	while (eg_list.size() > 0) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(5));
-		while (!egg_mutex.try_lock()) {
-			std::this_thread::sleep_for(std::chrono::microseconds(500));
-		}
-		std::cout << "From egg_manager thread: " << eg_list.front()->count_eggs() << std::endl;
-		eg_list.front()->set_eggs(eg_list.front()->count_eggs() - 1);
-		egg_mutex.unlock();
-	}	
+  std::chrono::time_point<std::chrono::steady_clock> now;
+  while (eg_list.size() > 0) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  while (!egg_mutex.try_lock()) {
+  std::this_thread::sleep_for(std::chrono::microseconds(500));
+  }
+  std::cout << "From egg_manager thread: " << eg_list.front()->count_eggs() << std::endl;
+  eg_list.front()->set_eggs(eg_list.front()->count_eggs() - 1);
+  egg_mutex.unlock();
+  }
 }
 
 // Declare and define upkeep
 void upkeep(std::list<class Chicken *> ckn_list, std::list<class Egg *> eg_list, float * cash_difference) {
-	std::chrono::time_point<std::chrono::steady_clock> now;
-	while ( *cash_difference > 0 ) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(700));
-		while (!chicken_mutex.try_lock() && !egg_mutex.try_lock()) {
-			std::this_thread::sleep_for(std::chrono::microseconds(500));
-		}
-		if ( ckn_list.front()->count_chickens() > 15 ) {
-			*cash_difference += ckn_list.front()->sell_chickens(ckn_list.front()->count_chickens() - 15);
-		}
-		chicken_mutex.unlock();
-		egg_mutex.unlock();
-	}
+  std::chrono::time_point<std::chrono::steady_clock> now;
+  while ( *cash_difference > 0 ) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(700));
+  while (!chicken_mutex.try_lock() && !egg_mutex.try_lock()) {
+  std::this_thread::sleep_for(std::chrono::microseconds(500));
+  }
+  if ( ckn_list.front()->count_chickens() > 15 ) {
+  *cash_difference += ckn_list.front()->sell_chickens(ckn_list.front()->count_chickens() - 15);
+  }
+  chicken_mutex.unlock();
+  egg_mutex.unlock();
+  }
 }
 
 int main(int argc, char const * argv[]) {
-	// Setup File I/O
-	std::string const read_filename = "StarterFarm.txt";
-	std::string const write_filename = "FinalFarm.txt";
-	std::fstream read_file(read_filename, read_file.in);
-	std::fstream write_file(write_filename, write_file.out);
-	
-	// Setup the Chicken Coop
-	std::string string_chicken, string_eggs, string_debt, string_goal;
-	unsigned int starter_chickens, starter_eggs, amount_of_allowable_debt, profit_goal;
-	float farm_cash = 0.0;
-	float cash_difference = (float) profit_goal - farm_cash;
-	
-	// Setup the Accounts
-	std::chrono::time_point<std::chrono::steady_clock> start_time, now, end_time;
-	std::chrono::duration<double> diff;
+  // Setup File I/O
+  std::string const read_filename = "StarterFarm.txt";
+  std::string const write_filename = "FinalFarm.txt";
+  std::fstream read_file(read_filename, read_file.in);
+  std::fstream write_file(write_filename, write_file.out);
 
-	// Read in given variables and start time
-	if (!read_file.is_open()) {
-		std::cout << "Failed to open " << read_filename << std::endl;
-	} else {
-		// Read the file into variables
-		read_file >> starter_chickens;
-	        read_file.ignore(256, ',');
-	        read_file >> starter_eggs;
-		read_file.ignore(256, ',');
-		read_file >> amount_of_allowable_debt;
-		read_file.ignore(256, ',');
-		read_file >> profit_goal;
-		std::cout << starter_chickens << ", " << starter_eggs << ", " << amount_of_allowable_debt << ", " << profit_goal << std::endl;
-	}
-	
-	read_file.close();
+  // Setup the Chicken Coop
+  std::string string_chicken, string_eggs, string_debt, string_goal;
+  unsigned int starter_chickens, starter_eggs, amount_of_allowable_debt, profit_goal;
+  float farm_cash = 0.0;
+  float cash_difference = (float) profit_goal - farm_cash;
 
-	// Setup the chicken farm
-	std::vector<class Chicken *> chicken_vector;
-	std::vector<class Egg *> egg_vector;
-	class Chicken * chicken_ptr = new class Chicken(starter_chickens, start_time);
-	class Egg * egg_ptr = new class Egg(starter_eggs, start_time);
-	
-	chicken_vector.push_back(chicken_ptr);
-	egg_vector.push_back(egg_ptr);
+  // Setup the Accounts
+  std::chrono::time_point<std::chrono::steady_clock> start_time, now, end_time;
+  std::chrono::duration<double> diff;
 
-	// Setup coop and carton
-	std::list<class Chicken *> * coop = new std::list<class Chicken *>();
-	std::list<class Egg *> * carton = new std::list<class Egg *>();
+  // Read in given variables and start time
+  if (!read_file.is_open()) {
+  std::cout << "Failed to open " << read_filename << std::endl;
+  } else {
+  // Read the file into variables
+  read_file >> starter_chickens;
+          read_file.ignore(256, ',');
+          read_file >> starter_eggs;
+  read_file.ignore(256, ',');
+  read_file >> amount_of_allowable_debt;
+  read_file.ignore(256, ',');
+  read_file >> profit_goal;
+  std::cout << starter_chickens << ", " << starter_eggs << ", " << amount_of_allowable_debt << ", " << profit_goal << std::endl;
+  }
 
-	coop->push_back(chicken_ptr);
-	carton->push_back(egg_ptr);
+  read_file.close();
 
-	std::cout << "Farm setup!" << std::endl << "Chickens: " << chicken_ptr->count_chickens() << std::endl << "Eggs: " << egg_ptr->count_eggs() << std::endl;
-	
-	// Setup the threads
-	std::thread chicken_thread, egg_thread, upkeep_thread;
-	chicken_thread = std::thread(chicken_manager, coop);
-	egg_thread = std::thread(egg_manager, carton);
-	upkeep_thread = std::thread(upkeep, coop, carton, *cash_difference);
+  // Setup the chicken farm
+  std::vector<class Chicken *> chicken_vector = std::vector<class Chicken *>();
+  std::vector<class Egg *> egg_vector = std::vector<class Egg *>();
+  class Chicken * chicken_ptr = new class Chicken(starter_chickens, start_time);
+  class Egg * egg_ptr = new class Egg(starter_eggs, start_time);
 
-	// Start the timer
-	start_time = std::chrono::steady_clock::now();
-	
-	// Do the stuff
-	now = std::chrono::steady_clock::now();
-	diff = now - start_time;
-	class Egg* nweg_ptr = new class Egg();
-	class Chicken* nwchk_ptr = new class Chicken();
+  chicken_vector.push_back(chicken_ptr);
+  egg_vector.push_back(egg_ptr);
 
-       	chicken_ptr->lay_eggs(nweg_ptr, now);
-	egg_ptr->hatch(nwchk_ptr, now);
-	std::cout << "Eggs laid!" << std::endl << "Eggs hatched!" << std::endl << "At time: " << diff.count() << std::endl;
+  // Setup coop and carton
+  std::list<class Chicken *> *coop = new std::list<class Chicken *>();
+  std::list<class Egg *> *carton = new std::list<class Egg *>();
 
-	// Join the threads
-	chicken_thread.join();
-	egg_thread.join();
-	upkeep_thread.join();
+  coop->push_back(chicken_ptr);
+  carton->push_back(egg_ptr);
 
-	// Write out results
-	write_file << chicken_ptr->count_chickens() << "," << egg_ptr->count_eggs() << "," << (unsigned int)  farm_cash;
-	// Clean up
-	end_time = std::chrono::steady_clock::now();
-	diff = end_time - start_time;
-	std::cout << "Run time: " << diff.count() << std::endl;
-	write_file.close();
+  std::cout << "Farm setup!" << std::endl << "Chickens: " << chicken_ptr->count_chickens() << std::endl << "Eggs: " << egg_ptr->count_eggs() << std::endl;
 
-	return 0;
+  // Setup the threads
+  std::thread chicken_thread, egg_thread, upkeep_thread;
+  chicken_thread = std::thread(chicken_manager, *coop);
+  egg_thread = std::thread(egg_manager, *carton);
+  upkeep_thread = std::thread(upkeep, *coop, *carton, &cash_difference);
+
+  // Start the timer
+  start_time = std::chrono::steady_clock::now();
+
+  // Do the stuff
+  now = std::chrono::steady_clock::now();
+  diff = now - start_time;
+  class Egg* nweg_ptr = new class Egg();
+  class Chicken* nwchk_ptr = new class Chicken();
+
+         chicken_ptr->lay_eggs(nweg_ptr, now);
+  egg_ptr->hatch(nwchk_ptr, now);
+  std::cout << "Eggs laid!" << std::endl << "Eggs hatched!" << std::endl << "At time: " << diff.count() << std::endl;
+
+  // Join the threads
+  chicken_thread.join();
+  egg_thread.join();
+  upkeep_thread.join();
+
+  // Write out results
+  write_file << chicken_ptr->count_chickens() << "," << egg_ptr->count_eggs() << "," << (unsigned int)  farm_cash;
+  // Clean up
+  end_time = std::chrono::steady_clock::now();
+  diff = end_time - start_time;
+  std::cout << "Run time: " << diff.count() << std::endl;
+  write_file.close();
+
+  return 0;
 }
