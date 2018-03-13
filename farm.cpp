7f1faf4ac39a1ebb2a1f2af355175b112bc762d9
Kelan Ige <igek@hawaii.edu>
2018-03-12 20:52:56 -0400

    Removed cout statements

--- farm.cpp
+++ farm.cpp
@@ -45,7 +45,6 @@ int main (int argc, char const * argv[]) {
   read_file >> amount_of_allowable_debt;
   read_file.ignore(256, ',');
   read_file >> profit_goal;
-  std::cout << starter_chickens << ", " << starter_eggs << ", " << amount_of_allowable_debt << ", " << profit_goal << std::endl;
   }
 
   read_file.close();
@@ -68,7 +67,7 @@ int main (int argc, char const * argv[]) {
   prev_upkeep = now;
 
   while (cash_difference > 0) {
-    // std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
 
     now = std::chrono::steady_clock::now();
     diff = now - start_time;
@@ -78,8 +77,7 @@ int main (int argc, char const * argv[]) {
     diff_upkeep = now - prev_upkeep;
 
     // Lay eggs
-	if (diff_chicken >= std::chrono::milliseconds(10)) {
-		std::cout << diff.count() << " : Chickens laying eggs..." << std::endl;
+	if (diff_chicken >= std::chrono::milliseconds(6)) {
 		class Egg * new_egg = new class Egg();
 
 		new_egg->setCount(broods->getCount());
@@ -89,8 +87,7 @@ int main (int argc, char const * argv[]) {
 	}
 
     // Hatch eggs
-	if (diff_egg >= std::chrono::milliseconds(100)) {
-		std::cout << diff.count() << " : Eggs hatching..." << std::endl;
+	if (diff_egg >= std::chrono::milliseconds(96)) {
         std::list<class Chicken *> * temp = clutches->hatchAll();
         if (!temp->empty()) {
     		broods->getList()->insert(broods->getList()->end(), temp->begin(), temp->end());
@@ -100,12 +97,11 @@ int main (int argc, char const * argv[]) {
 	}
 
     // If profit_goal can be reached, sell
-	if (diff_sell >= std::chrono::milliseconds(25)) {
+	if (diff_sell >= std::chrono::milliseconds(21)) {
 		float revenue_estimate = broods->getRevenueEstimate() + clutches->getRevenueEstimate();
 		float cash = 0;
 
 		if (revenue_estimate > cash_difference) {
-			std::cout << diff.count() << " : Selling..." << std::endl;
 			cash = broods->sell((unsigned int)((cash_difference - cash) / 10));
             cash += clutches->sell((unsigned int)((cash_difference - cash) / 0.25));
 			cash_difference -= cash;
@@ -116,8 +112,7 @@ int main (int argc, char const * argv[]) {
 	}
 
     // Pay Upkeep
-	if (diff_upkeep >= std::chrono::milliseconds(700)) {
-		std::cout << diff.count() << " : Upkeep..." << std::endl;
+	if (diff_upkeep >= std::chrono::milliseconds(696)) {
 		float cash = 0;
         
         // Sell off excess chickens ( > 25 )
@@ -127,37 +122,25 @@ int main (int argc, char const * argv[]) {
 
         // Make sure debt limit is not exceeded
         if ( (farm_cash - broods->getUpkeepEstimate() - clutches->getUpkeepEstimate()) < -1 * amount_of_allowable_debt ) {
-                std::cout << diff.count() << " : Welp, you're going to exceed the debt limit." << std::endl;
                 float debt_difference = (broods->getUpkeepEstimate() + clutches->getUpkeepEstimate()) - (farm_cash + amount_of_allowable_debt);
                 float temp_cash = broods->sell((unsigned int)(debt_difference / 10));
                 temp_cash += clutches->sell((unsigned int)((debt_difference - temp_cash) / 0.25));
                 cash += temp_cash;
-
-                std::cout << diff.count() << " : Disaster avoided! You came this close to the debt limit: " << farm_cash << std::endl;
         } 
 
-//        cash -= (broods->getUpkeepEstimate() + clutches->getUpkeepEstimate());
         cash_difference -= cash;
         farm_cash += cash;
-
-        std::cout << diff.count() << " : Cash Difference : $" << cash_difference << std::endl;
-        std::cout << diff.count() << " : Farm Cash : $" << farm_cash << std::endl;
         prev_upkeep = now;
     }
   }
 
   // Write out results
-  write_file << broods->getCount() << "," << clutches->getCount() << "," << (unsigned int)  farm_cash;
+  write_file << broods->getCount() << ", " << clutches->getCount() << ", " << (unsigned int)  farm_cash;
   write_file.close();
  
   // Clean up
   end_time = std::chrono::steady_clock::now();
   diff = end_time - start_time;
-  std::cout << "Days to reach goal: " << days << std::endl;
-  std::cout << "Final chickens: " << broods->getCount() << std::endl;
-  std::cout << "Final eggs: " << clutches->getCount() << std::endl;
-  std::cout << "Final cash: " << (unsigned int)farm_cash << std::endl;
-  std::cout << "Run time: " << diff.count() << std::endl;
 
   return days;
 }
